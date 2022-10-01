18371ca3777d992bf025c4a5000942a963dd5961
Marcos Daniel Saguier <matecondani@gmail.com>
2022-10-01 12:37:24 -0300

    initial commit

---
+++ src/main/java/com/marcos/saguier/msagecalc/MsAgeCalcApplication.java
@@ -0,0 +1,13 @@
+package com.marcos.saguier.msagecalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAgeCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAgeCalcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcos/saguier/msagecalc/MsAgeCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcos.saguier.msagecalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAgeCalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
